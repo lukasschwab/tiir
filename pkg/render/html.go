d6086894944c7e06e863428597c1227a0bccb8f4
Lukas Schwab <lukas.schwab@gmail.com>
2023-03-31 00:49:02 -0700

    Add date headings to HTML template
    This is a bodge, but it gets me closer to the legacy HTML appearance.
    
    The issue: I don't like that appearance. The date headers are
    interruptive. I think they'd be better-applied as some :before element
    for the first td in a group...

--- pkg/render/html.go
+++ pkg/render/html.go
@@ -3,8 +3,9 @@ package render
 import (
 	_ "embed"
 	"fmt"
-	"html/template"
 	"io"
+	"text/template"
+	"time"
 
 	"github.com/lukasschwab/tiir/pkg/text"
 )
@@ -17,8 +18,33 @@ func HTML(texts []*text.Text, to io.Writer) error {
 	if err != nil {
 		return fmt.Errorf("error parsing template: %w", err)
 	}
-	if err := tmpl.Execute(to, texts); err != nil {
+	if err := tmpl.Execute(to, byDate(texts)); err != nil {
 		return fmt.Errorf("error executing template: %w", err)
 	}
 	return nil
+}
+
+type dateGroup struct {
+	Key   string
+	Texts []*text.Text
+}
+
+func byDate(texts []*text.Text) []*dateGroup {
+	aggregate := []*dateGroup{}
+
+	key := func(t time.Time) string {
+		return t.Format("January 2, 2006")
+	}
+
+	// Assumes texts are sorted by Timestamp descending.
+	var currentGroup *dateGroup
+	for _, t := range texts {
+		if k := key(t.Timestamp); currentGroup != nil && k == currentGroup.Key {
+			currentGroup.Texts = append(currentGroup.Texts, t)
+		} else {
+			currentGroup = &dateGroup{Key: k, Texts: []*text.Text{t}}
+			aggregate = append(aggregate, currentGroup)
+		}
+	}
+	return aggregate
 }
