8788060ccc16d36562d82325a050c9b7dfa5410a
Lukas Schwab <lukas.schwab@gmail.com>
2023-03-30 23:59:15 -0700

    Shut down server gracefully: close service

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/lukasschwab/tiir/pkg/render"
-	"github.com/lukasschwab/tiir/pkg/store"
 	"github.com/lukasschwab/tiir/pkg/text"
 	"github.com/lukasschwab/tiir/pkg/tir"
 )
@@ -26,6 +27,9 @@ var (
 func main() {
 	app := fiber.New()
 
+	configuredService, _ := tir.FromConfig()
+	defer configuredService.Close()
+
 	// Inbound logging.
 	app.Use(logger.New())
 
@@ -36,7 +40,6 @@ func main() {
 			c.Status(fiber.StatusBadRequest)
 			return fmt.Errorf("error parsing request body: %w", err)
 		}
-		configuredService, _ := service()
 		if created, err := configuredService.Create(t); err != nil {
 			return fmt.Errorf("error writing record: %w", err)
 		} else {
@@ -58,7 +61,6 @@ func main() {
 			return fmt.Errorf("error parsing request body: %w", err)
 		}
 
-		configuredService, _ := service()
 		if updated, err := configuredService.Update(id, updates); err != nil {
 			return fmt.Errorf("error updating record: %w", err)
 		} else {
@@ -74,7 +76,6 @@ func main() {
 			return errors.New("update request must specify record ID")
 		}
 
-		configuredService, _ := service()
 		if deleted, err := configuredService.Delete(id); err != nil {
 			return fmt.Errorf("error deleting record: %w", err)
 		} else {
@@ -85,7 +86,6 @@ func main() {
 	// List all texts.
 	app.Get("/texts", func(c *fiber.Ctx) error {
 		// FIXME: handle errors.
-		configuredService, _ := service()
 		texts, _ := configuredService.List()
 
 		renderer := listRenderer[c.Query("format", "plain")]
@@ -93,16 +93,20 @@ func main() {
 		return nil
 	})
 
-	log.Fatal(app.Listen(":3000"))
-}
+	go func() {
+		if err := app.Listen(":3000"); err != nil {
+			log.Fatalf("shutting down: %v", err)
+		}
+	}()
 
-func service() (*tir.Service, error) {
-	// FIXME: placeholder service.
-	if home, err := os.UserHomeDir(); err != nil {
-		return nil, fmt.Errorf("error getting user home directory: %v", err)
-	} else if store, err := store.UseFile(home + "/.tir.json"); err != nil {
-		return nil, fmt.Errorf("error opening tir file: %v", err)
-	} else {
-		return &tir.Service{Store: store}, nil
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c // Block main thread until interrupt.
+	log.Printf("Gracefully shutting down...")
+	_ = app.Shutdown()
+	if err := configuredService.Close(); err != nil {
+		log.Printf("Error closing service: %v", err)
 	}
+	log.Printf("Shutdown")
 }
